ad0e59c0de22910e6a5beca975971f8225b874d9
HridayMahmud <hridaymahmud01@gmail.com>
2025-03-07 21:03:59 +0600

    pyramid bar chart done

--- src/Components/PagesToRead/PagesToRead.jsx
+++ src/Components/PagesToRead/PagesToRead.jsx
@@ -1,13 +1,50 @@
 import React from 'react'
 
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "The Great Gatsby", value: 192 },
+  { name: "To Kill a Mockingbird", value: 281 },
+  { name: "1984", value: 328 },
+  { name: "The Alchemist", value: 177 },
+  { name: "Pride and Prejudice", value: 279 },
+];
+
 const PagesToRead = () => {
   return (
-    <div>
-      <div className="text-2xl font-semibold text-center">
-               <h1>Graph chart of how many pages have been read</h1>
-      </div>
-    </div>
-  )
-}
+   <div className="mx-12">
+     <ResponsiveContainer width="100%" height={400}>
+      <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar
+          dataKey="value"
+          fill="#8884d8"
+          shape={(props) => (
+            <path
+              d={`M${props.x},${props.y + props.height} 
+                 L${props.x + props.width / 2},${props.y} 
+                 L${props.x + props.width},${props.y + props.height} Z`}
+              fill={props.fill}
+            />
+          )}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+   </div>
+  );
+};
 
-export default PagesToRead
+
+
+export default PagesToRead;
